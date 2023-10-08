ad4ebdccaaa391c13c18a088892f6ca480f4cd33
Your Name <ulainn400@gmail.com>
2023-10-08 23:54:45 +0500

    done

---
+++ index.js
@@ -0,0 +1,82 @@
+
+
+window.addEventListener('load', () => {
+    const form = document.querySelector("#new-task-form");
+    const input = document.querySelector("#new-task-input");
+    const list_el = document.querySelector("#tasks");
+
+    form.addEventListener('submit', (e) => {
+        e.preventDefault();
+
+        const task = input.value;
+
+        if (task.trim() === "") {
+            alert("Task cannot be empty!");
+            return;
+        }
+
+        const task_el = document.createElement('div');
+        task_el.classList.add('task');
+
+        const task_content_el = document.createElement('div');
+        task_content_el.classList.add('content');
+
+        task_el.appendChild(task_content_el);
+
+        const task_input_el = document.createElement('input');
+        task_input_el.classList.add('text');
+        task_input_el.type = 'text';
+        task_input_el.value = task;
+        task_input_el.setAttribute('readonly', 'readonly');
+
+        task_content_el.appendChild(task_input_el);
+
+        const task_actions_el = document.createElement('div');
+        task_actions_el.classList.add('actions');
+
+        const task_edit_el = document.createElement('button');
+        task_edit_el.classList.add('edit');
+        task_edit_el.innerText = 'Edit';
+
+        const task_delete_el = document.createElement('button');
+        task_delete_el.classList.add('delete');
+        task_delete_el.innerText = 'Delete';
+
+        task_actions_el.appendChild(task_edit_el);
+        task_actions_el.appendChild(task_delete_el);
+
+        task_el.appendChild(task_actions_el);
+
+        list_el.appendChild(task_el);
+
+        input.value = '';
+    });
+
+    // Event listener for the edit button outside of the form submit event
+    list_el.addEventListener('click', (e) => {
+        const target = e.target;
+
+        if (target.classList.contains('edit')) {
+            const task_el = target.closest('.task');
+            const task_input_el = task_el.querySelector('.text');
+
+            if (target.innerText.toLowerCase() === "edit") {
+                const confirmEdit = confirm("Are you sure you want to edit this task?");
+                if (confirmEdit) {
+                    target.innerText = "Save";
+                    task_input_el.removeAttribute("readonly");
+                    task_input_el.focus();
+                }
+            } else {
+                target.innerText = "Edit";
+                task_input_el.setAttribute("readonly", "readonly");
+            }
+        } else if (target.classList.contains('delete')) {
+            const task_el = target.closest('.task');
+            const confirmDelete = confirm("Are you sure you want to delete this task?");
+            if (confirmDelete) {
+                list_el.removeChild(task_el);
+            }
+        }
+    });
+});
